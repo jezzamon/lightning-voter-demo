b49cfbe6af336a507c78f6abbdebe336afbe32a2
jezzamondev <jezzamondev@gmail.com>
2018-02-10 11:53:29 -0500

    refactor scope attr to be nested in bindToController for clearer / cleaner code

--- public/home/unreviewedTalk.js
+++ public/home/unreviewedTalk.js
@@ -1,13 +1,13 @@
 angular.module('app').directive('unreviewedTalk', function() {
   return {
     templateUrl: '/home/unreviewedTalk.html',
-    scope: {
+    scope: {},
+    controllerAs: '$ctrl', // using $ctrl as opposed to recommended 'vm', for ng 1.5 conventions
+    bindToController: {
       session: '=',
       voteYes: '&',
       voteNo: '&'
     },
-    controllerAs: '$ctrl', // using $ctrl as opposed to recommended 'vm', for ng 1.5 conventions
-    bindToController: true,
     controller: function() {
       
       this.yes = function() {
