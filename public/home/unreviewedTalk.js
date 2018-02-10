8a987c9f9f047dea6daf4ad78481e41ee0659f57
jezzamondev <jezzamondev@gmail.com>
2018-02-10 11:51:36 -0500

    refator to remove $scope, using controllerAs and bindToController

--- public/home/unreviewedTalk.js
+++ public/home/unreviewedTalk.js
@@ -6,13 +6,15 @@ angular.module('app').directive('unreviewedTalk', function() {
       voteYes: '&',
       voteNo: '&'
     },
-    controller: function($scope) {
+    controllerAs: '$ctrl', // using $ctrl as opposed to recommended 'vm', for ng 1.5 conventions
+    bindToController: true,
+    controller: function() {
       
-      $scope.yes = function() {
+      this.yes = function() {
         this.voteYes();
       }
       
-      $scope.no = function() {
+      this.no = function() {
         this.voteNo();
       }
     }
