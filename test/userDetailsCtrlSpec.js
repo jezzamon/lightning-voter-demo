2dab6ba53f41282660623dc319bc5f318adec176
jezzamondev <jezzamondev@gmail.com>
2018-02-11 16:15:29 -0500

    fixing tests for converted controller userDetail

--- test/userDetailsCtrlSpec.js
+++ test/userDetailsCtrlSpec.js
@@ -1,20 +1,20 @@
 
-describe('userDetailsCtrlCtrl', function() {
+describe('userDetails', function() {
   var $ctrlCnst; 
   
   beforeEach(module('app'));
   
-  beforeEach(inject(function($controller) {
-    $ctrlCnst = $controller;
+  beforeEach(inject(function($componentController) {
+    $ctrlCnst = $componentController;
   }));
   
   it('should set the user on the controller to the matching user by id', function() {
     
-    var ctrl = $ctrlCnst('userDetailsCtrl',
-      {
-        allUsers: [{id:1,name:'wrong'},{id:3,name:'correct'}], 
-        '$routeParams': {id:3}
-      })
+    var ctrl = $ctrlCnst('userDetails',
+      // two params needed 
+      { $routeParams : {id:3}}, //1) paramaters 
+      { allUsers: [{id:1,name:'wrong'},{id:3,name:'correct'}]} //2) bindings passed 
+    )
       
     expect(ctrl.user.name).toBe('correct');
   })
