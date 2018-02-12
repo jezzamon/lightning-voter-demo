9884636a6a2f3fe1b5a9e7df5014a86e2817310f
jezzamondev <jezzamondev@gmail.com>
2018-02-11 19:49:32 -0500

    convert createNewSession controller to components

--- public/routes.js
+++ public/routes.js
@@ -79,9 +79,7 @@ app.config(function($routeProvider) {
       }
     })
     .when('/createsession', {
-      controller: 'createNewSessionCtrl',
-      templateUrl: 'home/createNewSession.html',
-      controllerAs: 'vm',
+      template: '<create-new-session user-sessions="$resolve.userSessions"></create-new-session>',
       resolve: {
         userSessions: routeResolvers.userSessions,
       }
