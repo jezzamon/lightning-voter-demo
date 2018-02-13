9e8eb1f8baa72908f43fa763c2f5fa45c51f2dab
jezzamondev <jezzamondev@gmail.com>
2018-02-12 20:22:05 -0500

    switch Controller to ES6 Class

---
+++ public/security/login.ts
@@ -0,0 +1,45 @@
+angular.module('app').component('login', {
+  
+  templateUrl: '/security/login.html',
+  bindings: {},
+  controller: class LoginCtrl {
+    
+    constructor(
+      public $location, 
+      public currentIdentity, 
+      public auth, 
+      public toastr) {
+      
+        if(currentIdentity.authenticated()) {
+              $location.path('/home');
+            }
+    }
+
+    // $location: any;
+    // auth: any;
+    // toastr: any;
+    // email: string;
+
+    // constructor($location, currentIdentity, auth, toastr) {
+    //   this.$location = $location;
+    //   this.auth = auth;
+    //   this.toastr = toastr;
+
+    //     if(currentIdentity.authenticated()) {
+    //       $location.path('/home');
+    //     }
+    // }
+    
+    login() {
+      this.auth.login({
+        username: this.email,
+        password: "pass"
+      }).then(() => {
+        this.$location.path('/home');
+      }, (err) => {
+        this.toastr.error(err);
+      })
+    }
+    
+  }
+})
