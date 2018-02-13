35137d176698c339abed2fe462f4e8311c0a9ba1
jezzamondev <jezzamondev@gmail.com>
2018-02-12 20:47:46 -0500

    switch service to ES6 class

--- public/security/auth.js
+++ public/security/auth.js
@@ -1,56 +1,73 @@
-angular.module('app').factory('auth', function($q, $http, currentIdentity) {
-  return {
-    login: function(credentials) {
-      var dfd = $q.defer();
-      $http.post('/api/login', credentials).then(function(response) {
-        currentIdentity.setUser(response.data.user);
-        
-        dfd.resolve();
-      }, function(response) {
-        dfd.reject("Invalid Credentials");
-      })
-      return dfd.promise;
-    },
-    logout: function() {
-      var dfd = $q.defer();
-      $http.post('/api/logout').then(function(response) {
-        currentIdentity.clearUser();
-        dfd.resolve();
-      }, function(response) {
-        dfd.reject("Error Logging Out");
-      })
-      return dfd.promise;
-    },
-    
-    waitForAuth: function() {
-      var dfd = $q.defer();
-      $http.get('/api/currentIdentity').then(function(response) {
-        if(!!response.data) {
-          currentIdentity.setUser(response.data);
-        }
-        dfd.resolve(currentIdentity);
-      })
-      return dfd.promise;
-    },
-    
-    requireLogin: function() {
-      return this.waitForAuth().then(function() {
-        if(currentIdentity.authenticated()) {
-          return true;
-        } else {
-          return $q.reject('AUTH_REQUIRED');
-        }
-      })
-    },
-    
-    requireAdmin: function() {
-      return this.waitForAuth().then(function() {
-        if(currentIdentity.authenticated() && currentIdentity.currentUser.isAdmin) {
-          return true;
-        } else {
-          return $q.reject('AUTH_REQUIRED');
-        }
-      })
+angular.module('app').service('auth', (function () {
+    // $q: any;
+    // $http: any;
+    // currentIdentity: any;
+    // constructor($q, $http, currentIdentity) {
+    //   this.$q = $q;
+    //   this.$http = $http;
+    //   this.currentIdentity = currentIdentity;
+    // }
+    // TS shortcut version
+    function Auth($q, $http, currentIdentity) {
+        this.$q = $q;
+        this.$http = $http;
+        this.currentIdentity = currentIdentity;
     }
-  }
-})
+    Auth.prototype.login = function (credentials) {
+        var _this = this;
+        var dfd = this.$q.defer();
+        this.$http.post('/api/login', credentials).then(function (response) {
+            _this.currentIdentity.setUser(response.data.user);
+            dfd.resolve();
+        }, function (response) {
+            dfd.reject("Invalid Credentials");
+        });
+        return dfd.promise;
+    };
+    Auth.prototype.logout = function () {
+        var _this = this;
+        var dfd = this.$q.defer();
+        this.$http.post('/api/logout').then(function (response) {
+            _this.currentIdentity.clearUser();
+            dfd.resolve();
+        }, function (response) {
+            dfd.reject("Error Logging Out");
+        });
+        return dfd.promise;
+    };
+    Auth.prototype.waitForAuth = function () {
+        var _this = this;
+        var dfd = this.$q.defer();
+        this.$http.get('/api/currentIdentity').then(function (response) {
+            if (!!response.data) {
+                _this.currentIdentity.setUser(response.data);
+            }
+            dfd.resolve(_this.currentIdentity);
+        });
+        return dfd.promise;
+    };
+    Auth.prototype.requireLogin = function () {
+        var _this = this;
+        return this.waitForAuth().then(function () {
+            if (_this.currentIdentity.authenticated()) {
+                return true;
+            }
+            else {
+                return _this.$q.reject('AUTH_REQUIRED');
+            }
+        });
+    };
+    Auth.prototype.requireAdmin = function () {
+        var _this = this;
+        return this.waitForAuth().then(function () {
+            if (_this.currentIdentity.authenticated() && _this.currentIdentity.currentUser.isAdmin) {
+                return true;
+            }
+            else {
+                return _this.$q.reject('AUTH_REQUIRED');
+            }
+        });
+    };
+    return Auth;
+}()));
+//# sourceMappingURL=auth.js.map
