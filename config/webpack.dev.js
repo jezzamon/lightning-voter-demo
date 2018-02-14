adf7ddfdef5669b697d31426a259dcf15efa779d
jezzamondev <jezzamondev@gmail.com>
2018-02-13 21:52:49 -0500

    Configuration for webpack

---
+++ config/webpack.dev.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const webpack = require('webpack');
+const helpers = require('./helpers');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const ENV = process.env.NODE.ENV = process.env.ENV = 'development';
+
+module.exports = {
+  entry: {
+    'ng1': './public/index.ts'
+  },
+
+  output: {
+    path: helpers.root('dist/dev'),
+    publicPath: '/',
+    filename: '[name].bundle.js',
+    chunkFilename: '[id].chunk.js'
+  },
+
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loaders: ['awesome-typescript-loader', 'angular2-template-loader']
+      },
+      {
+        test: /\.html$/,
+        loaders: 'html-loader'
+      }
+    ]
+  },
+
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'common',
+      miniChunks: Infinity
+    }),
+
+    new webpack.SourceMapDevToolPlugin({
+      "filename": "[file].map[query]",
+      "moduleFilenameTemplate": "[resource-path]",
+      "fallbackModuleFilenameTemplate": "[resource-path]?[hash]",
+      "sourceRoot": "webpack:///"
+    }),
+
+    new HtmlWebpackPlugin({
+      template: 'config/index.html'
+    }),
+
+    new webpack.DefinePlugin({
+      'process.env': {
+        'ENV': JSON.stringiy(ENV)
+      }
+    })
+  ]
+}
