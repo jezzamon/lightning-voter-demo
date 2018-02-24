3b2e4cb69bbb41fa96dd6d7982ba668f9161157c
jezzamondev <jezzamondev@gmail.com>
2018-02-24 15:50:11 -0500

    polyfills

--- public/main.ts
+++ public/main.ts
@@ -1,7 +1,4 @@
-//bring in shims to make sure angular2 works for web browsers
-import 'core-js/es7/reflect';
-import 'core-js/client/shim';
-import 'zone.js/dist/zone';
+
 
 
 // used to bootstrap ng2 app
